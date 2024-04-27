33d1892b099f10a5548fba9e16c06c0c73e176e7
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-27 20:13:11 -0300

    Update Grant.Revoke.sql

--- DCL/Grant.Revoke.sql
+++ DCL/Grant.Revoke.sql
@@ -10,8 +10,11 @@ Para que ocorra de fato a concessão de privilégios devemos criar um usuário:
   SELECT user,host,authentication_strings AS password FROM mysql.user;
   CREATE USER 'ALUNO'@'localhost' IDENTIFIED BY 'aluno';
   GRANT USAGE ON *.* TO 'ALUNO'@'localhost';
-  GRANT SELECT ON curso.* TO 'ALUNO'@'localhost';
+  GRANT SELECT ON curso.* TO 'ALUNO'@'localhost';    -- <-- Liberada a função SELECT  para o usuário --
   SHOW GRANTS FOR 'ALUNO'@'localhost';
+
+--  Liberar a função INSERT para o usuário: --
+GRANT INSERT ON curso.* TO 'ALUNO'@'%';
 
 
 
