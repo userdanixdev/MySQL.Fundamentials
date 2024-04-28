a159252bdfb92edeab8be88a8084366726e64eeb
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-28 00:40:08 -0300

    Update Transaction Control Language.sql

--- TCL/Transaction Control Language.sql
+++ TCL/Transaction Control Language.sql
@@ -5,3 +5,29 @@
     - COMMIT -- salvar o trabalho feito
     - SAVE TRANSACTION -- Um ponto de salvamento da transação para que mais tarde você possa efetuar um ROLLBACK, caso necessário
     - ROLLBACK -- Restaura banco de dados ao original desde o último commit.
+
+--------------------//---------------------------//----------------------//----------------------//---------------------//---------
+-- Exemplo inicial:
+-- TCL -  Transaction Control language -- 	
+CREATE TABLE cadastro
+(
+	nome VARCHAR (50) NOT NULL,
+	documento VARCHAR(20) NOT null
+	)
+	
+-- Iniciar  TRANSACTION --
+
+START TRANSACTION;
+
+-- INSERIR REGISTROS --
+INSERT INTO cadastro VALUES ('andre',789456123);	
+INSERT INTO cadastro VALUES ('daniel',789456124);	
+INSERT INTO cadastro VALUES ('matheus',789456125);	
+INSERT INTO cadastro VALUES ('thiago',789456126);	
+
+-- Se usar 'rollback' não irá retornar nenhum registro --
+-- O commit efetiva as informações a serem cadastradas --
+COMMIT ;
+
+SELECT * FROM cadastro
+--------------------------------//--------------------------//----------------------------//--------------------//-----------------//
