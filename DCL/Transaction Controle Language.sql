e001b38a2c2e0d41ab2bb799ded282c05781caa5
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-04-28 00:31:16 -0300

    Create Transaction Controle Language.sql

---
+++ DCL/Transaction Controle Language.sql
@@ -0,0 +1,9 @@
+-- DCL :: Transaction Control Language ::
+-- São usadas para gerenciar as mudanças feitas por instruções DML. Permite que as declarações sejam agrupadas de forma ordenada e lógica.
+-- Principais comandos:
+    - BEGIN TRANSACTION -- Inicia uma transação
+    - COMMIT -- salvar o trabalho feito
+    - SAVE TRANSACTION -- Um ponto de salvamento da transação para que mais tarde você possa efetuar um ROLLBACK, caso necessário
+    - ROLLBACK -- Restaura banco de dados ao original desde o último commit.
+
+
