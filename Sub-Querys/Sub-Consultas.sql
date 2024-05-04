cff0c85b20694a3387c0e698b2ed33c591299330
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-04 13:21:51 -0300

    Update Sub-Consultas.sql

--- Sub-Querys/Sub-Consultas.sql
+++ Sub-Querys/Sub-Consultas.sql
@@ -5,12 +5,52 @@ use sakila  -- Sakila é um banco de dados que é usado no MySQLworkbench como e
 
 -- Atenção: Tanto a tabela 'film_actor' quanto a 'actor' possuem o mesmo id - Existe as chaves que se relacionam, sendo assim:
 SELECT * FROM actor WHERE actor_id IN (SELECT actor_id FROM film_actor WHERE film_id = 1 )
+  +----------+------------+-----------+---------------------+
+| actor_id | first_name | last_name | last_update         |
++----------+------------+-----------+---------------------+
+|        1 | PENELOPE   | GUINESS   | 2006-02-15 04:34:33 |
+|       10 | CHRISTIAN  | GABLE     | 2006-02-15 04:34:33 |
+|       20 | LUCILLE    | TRACY     | 2006-02-15 04:34:33 |
+|       30 | SANDRA     | PECK      | 2006-02-15 04:34:33 |
+|       40 | JOHNNY     | CAGE      | 2006-02-15 04:34:33 |
+|       53 | MENA       | TEMPLE    | 2006-02-15 04:34:33 |
+|      108 | WARREN     | NOLTE     | 2006-02-15 04:34:33 |
+|      162 | OPRAH      | KILMER    | 2006-02-15 04:34:33 |
+|      188 | ROCK       | DUKAKIS   | 2006-02-15 04:34:33 |
+|      198 | MARY       | KEITEL    | 2006-02-15 04:34:33 |
++----------+------------+-----------+---------------------+
 
 -- Teremos assim o resultado de todos os atores que fizeram o film com código ID = 1 --
 
 -- Descobrir qual filme que atriz Penelope Guiness fez, sabendo que o código dela é ID = 1 -- 
 -- Boa prática utilizar os campos chaves por questão de performance do que campos texto --
-SELECT * FROM film WHERE film_id IN (SELECT film_id FROM film_actor WHERE actor_id = 1)
+SELECT * FROM film WHERE film_id IN (SELECT film_id FROM film_actor WHERE actor_id = 1);
+      -- com todas tabelas selecionadas a tabela irá ficar muito grande, sendo assim irei filtrar somente algumas colunas:
+SELECT title,description,release_year,length,rating FROM filme WHERE film_id IN (SELECT film_id FROM film_actor WHERE actor_id = 1);
++-----------------------+-------------------------------------------------------------------------------------------------------------------------+--------------+--------+--------+
+| title                 | description                                                                                                             | release_year | length | rating |
++-----------------------+-------------------------------------------------------------------------------------------------------------------------+--------------+--------+--------+
+| ACADEMY DINOSAUR      | A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies                        |         2006 |     86 | PG     |
+| ANACONDA CONFESSIONS  | A Lacklusture Display of a Dentist And a Dentist who must Fight a Girl in Australia                                     |         2006 |     92 | R      |
+| ANGELS LIFE           | A Thoughtful Display of a Woman And a Astronaut who must Battle a Robot in Berlin                                       |         2006 |     74 | G      |
+| BULWORTH COMMANDMENTS | A Amazing Display of a Mad Cow And a Pioneer who must Redeem a Sumo Wrestler in The Outback                             |         2006 |     61 | G      |
+| CHEAPER CLYDE         | A Emotional Character Study of a Pioneer And a Girl who must Discover a Dog in Ancient Japan                            |         2006 |     87 | G      |
+| COLOR PHILADELPHIA    | A Thoughtful Panorama of a Car And a Crocodile who must Sink a Monkey in The Sahara Desert                              |         2006 |    149 | G      |
+| ELEPHANT TROJAN       | A Beautiful Panorama of a Lumberjack And a Forensic Psychologist who must Overcome a Frisbee in A Baloon                |         2006 |    126 | PG-13  |
+| GLEAMING JAWBREAKER   | A Amazing Display of a Composer And a Forensic Psychologist who must Discover a Car in The Canadian Rockies             |         2006 |     89 | NC-17  |
+| HUMAN GRAFFITI        | A Beautiful Reflection of a Womanizer And a Sumo Wrestler who must Chase a Database Administrator in The Gulf of Mexico |         2006 |     68 | NC-17  |
+| KING EVOLUTION        | A Action-Packed Tale of a Boy And a Lumberjack who must Chase a Madman in A Baloon                                      |         2006 |    184 | NC-17  |
+| LADY STAGE            | A Beautiful Character Study of a Woman And a Man who must Pursue a Explorer in A U-Boat                                 |         2006 |     67 | PG     |
+| LANGUAGE COWBOY       | A Epic Yarn of a Cat And a Madman who must Vanquish a Dentist in An Abandoned Amusement Park                            |         2006 |     78 | NC-17  |
+| MULHOLLAND BEAST      | A Awe-Inspiring Display of a Husband And a Squirrel who must Battle a Sumo Wrestler in A Jet Boat                       |         2006 |    157 | PG     |
+| OKLAHOMA JUMANJI      | A Thoughtful Drama of a Dentist And a Womanizer who must Meet a Husband in The Sahara Desert                            |         2006 |     58 | PG     |
+| RULES HUMAN           | A Beautiful Epistle of a Astronaut And a Student who must Confront a Monkey in An Abandoned Fun House                   |         2006 |    153 | R      |
+| SPLASH GUMP           | A Taut Saga of a Crocodile And a Boat who must Conquer a Hunter in A Shark Tank                                         |         2006 |    175 | PG     |
+| VERTIGO NORTHWEST     | A Unbelieveable Display of a Mad Scientist And a Mad Scientist who must Outgun a Mad Cow in Ancient Japan               |         2006 |     90 | R      |
+| WESTWARD SEABISCUIT   | A Lacklusture Tale of a Butler And a Husband who must Face a Boy in Ancient China                                       |         2006 |     52 | NC-17  |
+| WIZARD COLDBLOODED    | A Lacklusture Display of a Robot And a Girl who must Defeat a Sumo Wrestler in A MySQL Convention                       |         2006 |     75 | PG     |
++-----------------------+-------------------------------------------------------------------------------------------------------------------------+--------------+--------+--------+
+  
 -- Aqui temos mais um exemplo de sub-query utilizando as chaves de relacionanmentos -- 
 -- Sendo assim, a consulta irá retornar todos os filmes que o ator com código 1 fez --
 
