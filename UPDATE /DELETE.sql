42d87b686510b3abf153a2b3aa77f539e8b3696c
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-04 22:59:25 -0300

    Create DELETE.sql

---
+++ UPDATE /DELETE.sql
@@ -0,0 +1,6 @@
+-- A cláusula delete apagará os dados existentes:
+DELETE  FROM minha_tabela -- Apagará todos os dados da tabela, é boa prática utilizar a cláusula WHERE para definir qual linha ou linhas apagar.
+DELETE FROM minha_tabela WHERE id = 1 -- Boa prática --
+-- Quanto tem uma tabela auto_increment , deverá depois de apagar os registros alterar a tabela para ela recomeçar a contagem
+DELETE FROM minha_tabela
+ALTER TABLE auto_increment = 1;
