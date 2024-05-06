5fed0a91434440fdc715fd1409931590ea93b27b
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-06 20:56:26 -0300

    Create UPPER.sql

---
+++ Função_caracteres.sql/UPPER.sql
@@ -0,0 +1,31 @@
+-- upper -- 
+use world;
+select name from country limit 5;
++-------------+
+| name        |
++-------------+
+| Aruba       |
+| Afghanistan |
+| Angola      |
+| Anguilla    |
+| Albania     |
++-------------+
+5 rows in set (0.01 sec)
+Select name,upper(name)retorno from country limit 10;
++----------------------+----------------------+
+| name                 | retorno              |
++----------------------+----------------------+
+| Aruba                | ARUBA                |
+| Afghanistan          | AFGHANISTAN          |
+| Angola               | ANGOLA               |
+| Anguilla             | ANGUILLA             |
+| Albania              | ALBANIA              |
+| Andorra              | ANDORRA              |
+| Netherlands Antilles | NETHERLANDS ANTILLES |
+| United Arab Emirates | UNITED ARAB EMIRATES |
+| Argentina            | ARGENTINA            |
+| Armenia              | ARMENIA              |
++----------------------+----------------------+
+10 rows in set (0.00 sec)
+
+
