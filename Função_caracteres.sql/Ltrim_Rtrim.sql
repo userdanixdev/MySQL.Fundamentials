b61d6e90cb3a4e0327bd5144a0c27b2fdf8dc253
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-06 16:14:17 -0300

    Update and rename Ltrim.sql to Ltrim_Rtrim.sql

--- Função_caracteres.sql/Ltrim_Rtrim.sql
+++ Função_caracteres.sql/Ltrim_Rtrim.sql
@@ -1,3 +1,4 @@
+-- A função para caracter 'ltrim' elimina os espaços do lado esquerdo --
 -- Para declarar uma variável é necessário inserir o 'SET' logo em seguida o '@':
 SET @variavel_1_ltrim='   Três espacos no início.';
 SELECT CONCAT('Texto sem espaço:',LTRIM(@variavel_1_ltrim))AS retorno
@@ -11,3 +12,16 @@ SELECT CONCAT('Texto com espaço:',@variavel_1_ltrim);
 | Texto com espaço:    Três espaços no início.     |
 +--------------------------------------------------+
 -- Com o ltrim, eliminamos os espaços dos caracteres -- 
+-- Função Rtrim elimina os espaços do lado direito --
+SET @variavel_2_rtrim='Três espaços no fina.   ';
+SELECT concat(rtrim(@variavel_2_rtrim),'Texto sem espaços: ') as retorno
+UNION ALL
+SELECT concat(@variavel_2_rtrim,'Texto com espaços: ');
+
++-----------------------------------------------+
+| retorno                                       |
++-----------------------------------------------+
+| Três espaços no fim:Texto sem espaços:        |
+| Três espaços no fim:   Texto com espaços:     |
++-----------------------------------------------+
+2 rows in set (0.00 sec)
