15c50d5a290a463bd478eb8449aa41cbf51e109a
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-06 17:34:28 -0300

    Update Replace.sql

--- Função_caracteres.sql/Replace.sql
+++ Função_caracteres.sql/Replace.sql
@@ -23,3 +23,30 @@ replace ('Isto é teste','teste','produção')para;
 +---------------+--------------------+
 1 row in set (0.00 sec)
 
+-- Replace em uma tabela --
+-- Usando a tabela world --
+use world;
+SELECT Continent, REPLACE (Continent,'South America','América do Sul')trocado from country where Continent = 'South America';
+
++---------------+-----------------+
+| Continent     | trocado         |
++---------------+-----------------+
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
+| South America | América do Sul  |
++---------------+-----------------+
+14 rows in set (0.00 sec)
+-- é somente uma consulta, os valores da tabela original não foram alterados, somente para fazer uma tradução para verificar melhor os dados --
+
+
