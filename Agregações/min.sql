8ee4b09e169eb7432e7635fce900a9d2cb15da86
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-05 21:42:07 -0300

    Update min.sql

--- Agregações/min.sql
+++ Agregações/min.sql
@@ -5,39 +5,40 @@ WHERE a.ano = 2014;
 -- Detalhe, se colocarmos o ALIAS na tabela, temos que colocar em todos os campos --
 -- O resultado é 822 -- A cidade co menor população do Brasil em 2014 -- 
 -- Para aprofundar mais os detalhes, podemos selecionar a coluna estado da base de dados senso:
-SELECT a.estado,MIN(a.populacao) as pop_min_estados FROM senso a where a.ano = 2014 GROUP BY estado;
+SELECT a.estado,MIN(a.populacao) as pop_min_estados FROM senso a where a.ano = 2014 GROUP BY estado ORDER BY 2;  <- -- ORDENADO PELA SEGUNDA COLUNA DA CONSULTA --
 -- Ao selecionar mais de uma coluna, obrigatoriamente agrupar com 'GROUP BY' --
 +---------------------+-----------------+
 | estado              | pop_min_estados |
 +---------------------+-----------------+
+| Minas Gerais        |             822 |
+| São Paulo           |             835 |
+| Mato Grosso         |            1000 |
+| Tocantins           |            1091 |
+| Goiás               |            1093 |
+| Piauí               |            1239 |
+| Rio Grande do Sul   |            1286 |
+| Santa Catarina      |            1389 |
+| Paraná              |            1416 |
+| Rio Grande do Norte |            1705 |
+| Paraíba             |            1767 |
+| Sergipe             |            2366 |
 | Rondônia            |            2432 |
+| Pernambuco          |            2884 |
+| Alagoas             |            2958 |
+| Mato Grosso do Sul  |            3005 |
+| Pará                |            3303 |
+| Maranhão            |            3539 |
+| Bahia               |            3644 |
+| Ceará               |            3812 |
+| Amapá               |            4404 |
+| Espírito Santo      |            4669 |
+| Rio de Janeiro      |            5380 |
 | Acre                |            5593 |
 | Amazonas            |            5599 |
 | Roraima             |            7309 |
-| Pará                |            3303 |
-| Amapá               |            4404 |
-| Tocantins           |            1091 |
-| Maranhão            |            3539 |
-| Piauí               |            1239 |
-| Ceará               |            3812 |
-| Rio Grande do Norte |            1705 |
-| Paraíba             |            1767 |
-| Pernambuco          |            2884 |
-| Alagoas             |            2958 |
-| Sergipe             |            2366 |
-| Bahia               |            3644 |
-| Minas Gerais        |             822 |
-| Espírito Santo      |            4669 |
-| Rio de Janeiro      |            5380 |
-| São Paulo           |             835 |
-| Paraná              |            1416 |
-| Santa Catarina      |            1389 |
-| Rio Grande do Sul   |            1286 |
-| Mato Grosso do Sul  |            3005 |
-| Mato Grosso         |            1000 |
-| Goiás               |            1093 |
 | Distrito Federal    |         2852372 |
 +---------------------+-----------------+
+27 rows in set (0.02 sec)
 
 -- Mínimo da região do Brasil --
 
