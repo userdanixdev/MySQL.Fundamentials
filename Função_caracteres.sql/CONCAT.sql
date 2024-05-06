c44c27fdc150670874f00e6936d39e748e162b2d
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-06 16:27:50 -0300

    Create CONCAT.sql

---
+++ Função_caracteres.sql/CONCAT.sql
@@ -0,0 +1,8 @@
+-- A função 'CONCAT' concatena valores de strings ou caracteres --
+SELECT CONCAT('My','S','QL')retorno;
++---------+
+| retorno |
++---------+
+| MySQL   |
++---------+
+1 row in set (0.00 sec)
