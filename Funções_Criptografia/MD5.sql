2eb7f9a30559ccb4bd01845ea9d61fecf6e2c134
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-10 16:41:30 -0300

    Update MD5.sql

--- Funções_Criptografia/MD5.sql
+++ Funções_Criptografia/MD5.sql
@@ -1,2 +1,17 @@
 -- Função MD5:
 select MD5
+
+
+
+
+-- Atualizando a senha do usuário André --
+UPDATE usuario set senha = md5('654321abc') WHERE id_usuario = 1
+-- Veja o resultado:
+select * from usuario;
++------------+-------+----------------------------------+
+| id_usuario | nome  | senha                            |
++------------+-------+----------------------------------+
+|          1 | Andre | b3ddd238d6184cfd458205957c535457 |
++------------+-------+----------------------------------+
+1 row in set (0.00 sec)
+
