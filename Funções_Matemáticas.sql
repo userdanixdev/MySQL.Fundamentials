be4739028b0ba57a6696f8c4b6dd3268ff7727fb
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-10 13:38:06 -0300

    Create Funções_Matemáticas.sql

---
+++ Funções_Matemáticas.sql
@@ -0,0 +1,17 @@
+-- Funções Matemáticas --
+-- abs  retornará o valor absoluto numérico --
+-- Exemplo:
+select abs(-1.0)val1,
+		    abs(0.0)val1,
+        abs(-2.0)val1,
+        abs(-9.0)val1,
+        abs(-5.4)val1,
+        abs(5.4)val1
+
++------+------+-----+------+
+| val1 | val2 | va3 | val4 |
++------+------+-----+------+
+|  1.0 |  2.0 | 9.0 |  5.4 |
++------+------+-----+------+
+1 row in set (0.00 sec)
+
