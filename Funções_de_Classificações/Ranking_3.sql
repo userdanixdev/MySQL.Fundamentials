53696d0d52336706128603c00eb3a9cf04e7f830
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-09 21:30:11 -0300

    Create Ranking_3.sql

---
+++ Funções_de_Classificações/Ranking_3.sql
@@ -0,0 +1,11 @@
+-- Ranking geral e ranking por Estado:
+-- Declarar variáveis contadoras :
+SET @PREV:= NULL;
+SET @pos_geral := 0;
+SET @pos_estado := 0;
+
+SELECT 
+@pos_geral := @pos_geral + 1 AS Posição_Geral,
+if(@PREV <> estado, @pos_estado :=1,@pos_estado := @pos_estado + 1)
+AS Posição_Estado,@PREV := estado AS Estado, nome_mun AS Município,populacao AS População FROM senso
+ORDER BY estado,populacao DESC
