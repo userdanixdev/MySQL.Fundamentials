241657596ff72a69a28ddb9da69a847e61379bb2
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-10 13:41:13 -0300

    Update Funções_Matemáticas.sql

--- Funções_Matemáticas.sql
+++ Funções_Matemáticas.sql
@@ -15,3 +15,18 @@ select abs(-1.0)val1,
 +------+------+-----+------+
 1 row in set (0.00 sec)
 
+-- A função Matemática 'rand' irá retornar valores aleatorios entre 0 e 1:	
+select rand() 
+union
+select rand()
+union
+select rand();
++---------------------+
+| rand()              |
++---------------------+
+| 0.38611418882954324 |
+| 0.07741686429587832 |
+| 0.22874178572440687 |
++---------------------+
+3 rows in set (0.01 sec)
+
