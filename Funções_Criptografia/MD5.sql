8100e70495c7fb9ddfbe6a93332d5e0ca2f7f8bb
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-10 16:43:29 -0300

    Update MD5.sql

--- Funções_Criptografia/MD5.sql
+++ Funções_Criptografia/MD5.sql
@@ -1,8 +1,29 @@
 -- Função MD5:
-select MD5
+select MD5('123456')retorno
++----------------------------------+
+| retorno                          |
++----------------------------------+
+| e10adc3949ba59abbe56e057f20f883e |
++----------------------------------+
+1 row in set (0.00 sec)
 
+ -- Criando tabela para teste:
+ CREATE TABLE usuario
+ (
+ id_usuario INT NOT NULL AUTO_INCREMENT,
+ nome VARCHAR (20),
+ senha VARCHAR (32),
+ PRIMARY KEY (id_usuario)
+ );
+ INSERT INTO usuario (nome,senha) VALUES ('Andre',MD5('123456'));
+ --Senha já está criptograda dentro do banco de dados
 
-
+select * from usuario;
++------------+-------+----------------------------------+
+| id_usuario | nome  | senha                            |
++------------+-------+----------------------------------+
+|          1 | Andre | e10adc3949ba59abbe56e057f20f883e |
++------------+-------+----------------------------------+
 
 -- Atualizando a senha do usuário André --
 UPDATE usuario set senha = md5('654321abc') WHERE id_usuario = 1
