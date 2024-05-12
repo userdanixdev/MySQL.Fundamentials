7d6dc911eb3b7153e7ce572bfa4164e74fbcba44
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-12 18:59:54 -0300

    Update Verificando_time_zone.sql

--- Função_data_hora/Verificando_time_zone.sql
+++ Função_data_hora/Verificando_time_zone.sql
@@ -1,2 +1,11 @@
 -- Para verificar a time_zone, como se fosse o fuso horário --
 select @@global.time_zone;
+select @@time_zone;
++-------------+
+| @@time_zone |
++-------------+
+| +00:00      |
++-------------+
+-- obs: Para problemas de fuso horário --
+-- set @@global.time_zone = '+3:00';
+
