363a114715bea40effc13951253dfa305fa31c82
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-12 18:54:27 -0300

    Create Verificando_lingua_padrao.sql

---
+++ Função_data_hora/Verificando_lingua_padrao.sql
@@ -0,0 +1,17 @@
+-- Para verificar a língua padrão da instalação do MySQL -- 
+SELECT @@lc_time_names;
++-----------------+
+| @@lc_time_names |
++-----------------+
+| en_US           |
++-----------------+
+-- Para modificar a língua padrão -- 
+SET @@lc_time_names =  'PT_br';
+Query OK, 0 rows affected (0.02 sec)
+SELECT @@lc_time_names;
++-----------------+
+| @@lc_time_names |
++-----------------+
+| pt_BR           |
++-----------------+
+1 row in set (0.00 sec)
