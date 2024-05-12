0f33e48d85f594879e83e98fbfb00cdeeee07e15
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-12 19:13:03 -0300

    Update date_time.sql

--- Função_data_hora/date_time.sql
+++ Função_data_hora/date_time.sql
@@ -57,6 +57,44 @@ select * from audit_log;
 |  2 | Atualização   | 2024-05-12 | 20:02:53 |
 +----+---------------+------------+----------+
 -- REPARE que a coluna foi atualizada -- 
+-- Exemplo 03: Retornar dia/mes/ano/hora/minutos/segundos em colunas separadas:
+select month(now())mes,
+       year(now())ano,
+       day(now())dia,
+       hour(now())horas,
+       minute(now())Minutos,
+       second(now())segundo;
++------+------+------+-------+--------+---------+
+| mes  | ano  | dia  | horas | minuto | segundo |
++------+------+------+-------+--------+---------+
+|    5 | 2024 |   12 |    22 |      4 |      37 |
++------+------+------+-------+--------+---------+
+1 row in set (0.01 sec)
+-- Usando a base de dados Sakila para outro exemplo, com a tabela film campo last_update -- Última atualização --
+select title,last_update as ultima_atualização, 
+  year(last_update)as ano,
+  month(last_update) as mês,
+  day(last_update)as Dia,
+  hour(last_update)as Hora,
+  minute(last_update)as Minuto,
+  second(last_update) as Segundo from film limit 10;
++------------------+----------------------+------+------+------+------+--------+---------+
+| title            | ultima_atualização   | ano  | mês  | Dia  | Hora | Minuto | Segundo |
++------------------+----------------------+------+------+------+------+--------+---------+
+| ACADEMY DINOSAUR | 2006-02-15 05:03:42  | 2006 |    2 |   15 |    5 |      3 |      42 |
+| ACE GOLDFINGER   | 2006-02-15 05:03:42  | 2006 |    2 |   15 |    5 |      3 |      42 |
+| ADAPTATION HOLES | 2006-02-15 05:03:42  | 2006 |    2 |   15 |    5 |      3 |      42 |
+| AFFAIR PREJUDICE | 2006-02-15 05:03:42  | 2006 |    2 |   15 |    5 |      3 |      42 |
+| AFRICAN EGG      | 2006-02-15 05:03:42  | 2006 |    2 |   15 |    5 |      3 |      42 |
+| AGENT TRUMAN     | 2006-02-15 05:03:42  | 2006 |    2 |   15 |    5 |      3 |      42 |
+| AIRPLANE SIERRA  | 2006-02-15 05:03:42  | 2006 |    2 |   15 |    5 |      3 |      42 |
+| AIRPORT POLLOCK  | 2006-02-15 05:03:42  | 2006 |    2 |   15 |    5 |      3 |      42 |
+| ALABAMA DEVIL    | 2006-02-15 05:03:42  | 2006 |    2 |   15 |    5 |      3 |      42 |
+| ALADDIN CALENDAR | 2006-02-15 05:03:42  | 2006 |    2 |   15 |    5 |      3 |      42 |
++------------------+----------------------+------+------+------+------+--------+---------+
+10 rows in set (0.00 sec)
+
+
 
 
 
