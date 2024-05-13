cdc64250b5031e303d7c8e967a6709190cea089f
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-05-12 21:23:01 -0300

    Update Date_diff.sql

--- Função_data_hora/Date_diff.sql
+++ Função_data_hora/Date_diff.sql
@@ -17,3 +17,20 @@ select title,last_update,datediff(now(),last_update) as diferença em dias from 
 +------------------+---------------------+-----------------+
 10 rows in set (0.00 sec)
 -- Repare que são 6662 dias de diferença a partir da data do filme até os dias de hoje -- 
+-- Podemos colocar uma coluna com a data atual para melhorar na visualização -- 
+select title,last_update,datediff(now(),last_update) as dif_dias, curdate() from filme limit 10; -- curdate() para data
++------------------+---------------------+-----------------+------------+
+| title            | last_update         | diferença_dias  | curdate()  |
++------------------+---------------------+-----------------+------------+
+| ACADEMY DINOSAUR | 2006-02-15 05:03:42 |            6662 | 2024-05-13 |
+| ACE GOLDFINGER   | 2006-02-15 05:03:42 |            6662 | 2024-05-13 |
+| ADAPTATION HOLES | 2006-02-15 05:03:42 |            6662 | 2024-05-13 |
+| AFFAIR PREJUDICE | 2006-02-15 05:03:42 |            6662 | 2024-05-13 |
+| AFRICAN EGG      | 2006-02-15 05:03:42 |            6662 | 2024-05-13 |
+| AGENT TRUMAN     | 2006-02-15 05:03:42 |            6662 | 2024-05-13 |
+| AIRPLANE SIERRA  | 2006-02-15 05:03:42 |            6662 | 2024-05-13 |
+| AIRPORT POLLOCK  | 2006-02-15 05:03:42 |            6662 | 2024-05-13 |
+| ALABAMA DEVIL    | 2006-02-15 05:03:42 |            6662 | 2024-05-13 |
+| ALADDIN CALENDAR | 2006-02-15 05:03:42 |            6662 | 2024-05-13 |
++------------------+---------------------+-----------------+------------+
+10 rows in set (0.00 sec)
